15aaa78a26739e9977e2b1f583f4de8a6149d738
Carter Harrison <carterjharrison@gmail.com>
2018-11-08 12:35:08 -0800

    correct random uuids

--- src/main/java/network/xyo/modbluetoothkotlin/XyoUuids.kt
+++ src/main/java/network/xyo/modbluetoothkotlin/XyoUuids.kt
@@ -9,17 +9,17 @@ object XyoUuids {
     /**
      * The primary GATT service that will be advertised.
      */
-    val XYO_SERVICE = UUID.fromString("133e4d1d-981d-12d9-9826-e75c724fbf45")!!
+    val XYO_SERVICE = UUID.fromString("0b0fccdb-9862-4b79-9a3d-663506838715")!!
 
     /**
      * The GATT characteristic to be written to. This will be in the XYO_SERVICE.
      */
-    val XYO_WRITE = UUID.fromString("abxe4d1d-981d-4439-9816-e75c7b424342")!!
+    val XYO_WRITE = UUID.fromString("6fc018ff-c50b-4fef-9953-81a6ee2e3203")!!
 
     /**
      * The GATT characteristic to be read from. This will be in the XYO_SERVICE.
      */
-    val XYO_READ = UUID.fromString("333e4d1d-924d-15d9-9256-e75c144fbf46")!!
+    val XYO_READ = UUID.fromString("b280ba98-54f2-4abc-ab6d-64e0e68c11b9")!!
 
 
 }
