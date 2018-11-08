a99b27139ade5be3b8669aca7c0364e5fa61b1ea
Carter Harrison <carterjharrison@gmail.com>
2018-11-08 12:26:34 -0800

    spatial uuids

--- src/main/java/network/xyo/modbluetoothkotlin/XyoUuids.kt
+++ src/main/java/network/xyo/modbluetoothkotlin/XyoUuids.kt
@@ -9,17 +9,17 @@ object XyoUuids {
     /**
      * The primary GATT service that will be advertised.
      */
-    val XYO_SERVICE = UUID.fromString("113e4d1d-981d-4ad9-9826-e75c7b4fbf45")!!
+    val XYO_SERVICE = UUID.fromString("133e4d1d-981d-12d9-9826-e75c724fbf45")!!
 
     /**
      * The GATT characteristic to be written to. This will be in the XYO_SERVICE.
      */
-    val XYO_WRITE = UUID.fromString("222e4d1d-981d-4ad9-9816-e75c7b4fbf42")!!
+    val XYO_WRITE = UUID.fromString("abxe4d1d-981d-4439-9816-e75c7b424342")!!
 
     /**
      * The GATT characteristic to be read from. This will be in the XYO_SERVICE.
      */
-    val XYO_READ = UUID.fromString("333e4d1d-981d-4ad9-9816-e75c7b4fbf46")!!
+    val XYO_READ = UUID.fromString("333e4d1d-924d-15d9-9256-e75c144fbf46")!!
 
 
 }
